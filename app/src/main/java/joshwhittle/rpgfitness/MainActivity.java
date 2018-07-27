04303c84bd408be88e31407439b9cf45e76d7d82
joshwhit99 <josh.whittle@live.com>
2018-07-27 17:17:32 -0400

    saving

--- app/src/main/java/joshwhittle/rpgfitness/MainActivity.java
+++ app/src/main/java/joshwhittle/rpgfitness/MainActivity.java
@@ -1,7 +1,8 @@
 package joshwhittle.rpgfitness;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView introTextView = (TextView) findViewById(R.id.IntroTextView);
+        TextView welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
+        //introTextView.setText("RPG Fitness");
+        introTextView.setText("Welcome to RPG Fitness, the app that shows you your attributes and helps to grow them");
+        welcomeTextView.setText("RPG Fitness");
     }
+
+
+
 }
