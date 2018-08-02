54eff115d861cbb01ef1b8c531b910c09c371f8d
joshwhit99 <josh.whittle@live.com>
2018-08-02 17:16:10 -0400

    saving

--- app/src/main/java/joshwhittle/rpgfitness/MainActivity.java
+++ app/src/main/java/joshwhittle/rpgfitness/MainActivity.java
@@ -1,7 +1,10 @@
 package joshwhittle.rpgfitness;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +18,17 @@ public class MainActivity extends AppCompatActivity {
         //introTextView.setText("RPG Fitness");
         introTextView.setText("Welcome to RPG Fitness, the app that shows you your attributes and helps to grow them");
         welcomeTextView.setText("RPG Fitness");
+        Button welcomeStatsButton = (Button)findViewById(R.id.welcomeStatsButton);
+
+        welcomeStatsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this,showStats.class));
+            }
+
+        });
     }
+
 
 
 
--- app/src/main/java/joshwhittle/rpgfitness/showStats.java
+++ app/src/main/java/joshwhittle/rpgfitness/showStats.java
@@ -1,7 +1,10 @@
 package joshwhittle.rpgfitness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class showStats extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class showStats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_stats);
+        Button buttonShowStatsBack = (Button)findViewById(R.id.buttonShowStatsBack);
+        buttonShowStatsBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(showStats.this,MainActivity.class));
+            }
+
+        });
     }
 }
