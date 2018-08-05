c639edca2e1746e980579c5d8bebce71707e5ddc
joshwhit99 <josh.whittle@live.com>
2018-08-05 16:58:51 -0400

    saving

--- app/src/main/java/joshwhittle/rpgfitness/CreateWorkout.java
+++ app/src/main/java/joshwhittle/rpgfitness/CreateWorkout.java
@@ -1,13 +1,43 @@
 package joshwhittle.rpgfitness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateWorkout extends AppCompatActivity {
-
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+  /*  public void WorkoutName(View w){
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        EditText editTextCreateWorkoutText1 = (EditText) findViewById(R.id.editTextCreateWorkoutText1);
+        editor.putString("WorkoutName",editTextCreateWorkoutText1.getText().toString());
+        editor.apply();
+   */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_workout);
+        //SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+       // EditText editTextCreateWorkoutText1 = (EditText) findViewById(R.id.editTextCreateWorkoutText1);
+       // String test = editTextCreateWorkoutText1.getText().toString();
+
+       // editor.putString("WorkoutName",editTextCreateWorkoutText1.getText().toString());
+       // editor.apply();
+        Button buttonCreateWorkoutAddExercise = (Button)findViewById(R.id.buttonCreateWorkoutAddExercise);
+        buttonCreateWorkoutAddExercise.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                EditText editTextCreateWorkoutText1 = (EditText) findViewById(R.id.editTextCreateWorkoutText1);
+                editor.putString("WorkoutName",editTextCreateWorkoutText1.getText().toString());
+                editor.apply();
+                startActivity(new Intent(CreateWorkout.this,Exercizes.class));
+            }
+        });
+
     }
+
 }
--- app/src/main/java/joshwhittle/rpgfitness/Exercizes.java
+++ app/src/main/java/joshwhittle/rpgfitness/Exercizes.java
@@ -1,13 +1,47 @@
 package joshwhittle.rpgfitness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exercizes extends AppCompatActivity {
+
+    //List<String> exerciseList = Arrays.asList(new String[]{"benchpress","squat","deadlift"});
+
+    private ListView lv;
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercizes);
+        SharedPreferences pref = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String WorkoutName = pref.getString("WorkoutName", null);
+        String[] exerciseList = {"benchpress","squat","deadlift"};
+
+        lv = (ListView) findViewById(R.id.listViewExcercisesList);
+
+        List<String> listViewExcercisesList = new ArrayList<String>();
+        for (int i =0; i< exerciseList.length;i++){
+            listViewExcercisesList.add(exerciseList[i]);
+
+        }
+
+        //listViewExcercisesList.add(WorkoutName);
+        //listViewExcercisesList.add("Squat");
+       // listViewExcercisesList.add("Deadlift");
+        TextView textViewExercisesExercises = (TextView)findViewById(R.id.textViewExercisesExercises);
+        textViewExercisesExercises.setText(WorkoutName);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,android.R.layout.simple_list_item_1,listViewExcercisesList);
+        lv.setAdapter(arrayAdapter);
+
     }
 }
